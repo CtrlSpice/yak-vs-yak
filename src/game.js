8aac48fc9f31e820386c161f170b2ec7ff44d8e7
Amelia Ardath <amelia.ardath@gmail.com>
2021-04-17 17:47:39 -0700

    Non-multiplayer game functionality.

---
+++ src/game.js
@@ -0,0 +1,115 @@
+export function checkWinCondition(gameGrid, rowIndex, columnIndex) {
+  //Check if the move that was just played wins the game
+  let winSequenceLength = 4;
+  let directions = [
+    { head: "W", tail: "E" }, // Horizontal
+    { head: "N", tail: "S" }, // Vertical
+    { head: "NE", tail: "SW" }, // + Diag
+    { head: "NW", tail: "SE" }, // - Diag
+  ];
+
+  // Concatonate the head and tail sequences with the current block.
+  for (let direction of directions) {
+    // Order doesn't matter here
+    let sequence = buildSequence(gameGrid, rowIndex, columnIndex, direction.head).concat(
+      [[rowIndex, columnIndex]],
+      buildSequence(gameGrid, rowIndex, columnIndex, direction.tail)
+    );
+
+    if (sequence.length >= winSequenceLength)
+      return { isWin: true, sequence: sequence };
+  }
+  return { isWin: false, sequence: null };
+}
+
+export function prepareNextMove(gameGrid, rowIndex, columnIndex, blueIsNext) {
+    let grid = gameGrid;
+
+    // Works with both odd and even number of columns, in case we want a bigger grid
+    let mid = Math.floor(grid.length / 2);
+
+    // Follow Connect Four rules with mid as the ingress point, 
+    if (columnIndex < mid && grid[rowIndex][columnIndex + 1] === "locked") {
+      // If the square to the right is locked, make it available
+      grid[rowIndex][columnIndex + 1] = "open";
+    } else if (
+      columnIndex > mid &&
+      grid[rowIndex][columnIndex - 1] === "locked"
+    ) {
+      // If the square to the left is locked, make it available
+      grid[rowIndex][columnIndex - 1] = "open";
+    }
+  
+    //Check for tie condition (no more open squares)
+    let isTie = !grid.some((row) => row.includes("open"));
+    if (isTie) {
+      return { isDone: true, winner: "tie" };
+    } else {
+      return { grid: grid, blueIsNext: !blueIsNext };
+    }
+  }
+
+// Builds an array of adjacent squares that have the same value
+function buildSequence(grid, rowIndex, columnIndex, direction, sequence = []) {
+  let seq = sequence;
+  let currentValue = grid[rowIndex][columnIndex];
+  console.table(currentValue, direction);
+  let nextValue;
+  let nextRow;
+  let nextColumn;
+
+  // This can be simplified. It's just easier to debug this way.
+  switch (direction) {
+    case "N":
+      nextRow = rowIndex - 1;
+      nextColumn = columnIndex;
+      break;
+
+    case "S":
+      nextRow = rowIndex + 1;
+      nextColumn = columnIndex;
+      break;
+
+    case "E":
+      nextRow = rowIndex;
+      nextColumn = columnIndex + 1;
+      break;
+
+    case "W":
+      nextRow = rowIndex;
+      nextColumn = columnIndex - 1;
+      break;
+
+    case "NE":
+      nextRow = rowIndex - 1;
+      nextColumn = columnIndex + 1;
+      break;
+
+    case "SW":
+      nextRow = rowIndex + 1;
+      nextColumn = columnIndex - 1;
+      break;
+
+    case "NW":
+      nextRow = rowIndex - 1;
+      nextColumn = columnIndex - 1;
+      break;
+
+    case "SE":
+      nextRow = rowIndex + 1;
+      nextColumn = columnIndex + 1;
+      break;
+
+    default:
+      break;
+  }
+
+  if (nextRow >= 0 && nextRow < grid.length) {
+    nextValue = grid[nextRow][nextColumn];
+    if (currentValue === nextValue) {
+      seq.push([nextRow, nextColumn]);
+      seq = buildSequence(grid, nextRow, nextColumn, direction, seq);
+    }
+  }
+  return seq;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,156 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { checkWinCondition, prepareNextMove } from "./game.js";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const gridWidth = 7;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Square(props) {
+  let src = "images/" + props.value + ".svg";
+  return (
+    <button key={props.position} className="square" onClick={props.onClick}>
+      <img className="meeple" src={src} alt={props.value} />
+    </button>
+  );
+}
+
+function TurnStatus(props) {
+  let currentPlayer = props.currentPlayer + " yack";
+  return (
+    <p>
+      It's
+      <span className={"subtitle " + props.currentPlayer}>
+        {" " + currentPlayer}
+      </span>
+      's turn.
+    </p>
+  );
+}
+
+function FinalStatus(props) {
+  let winner = "";
+
+  switch (props.winner) {
+    case "blue":
+      winner = " Blue Yak ";
+      break;
+    case "orange":
+      winner = " Orange Yak ";
+      break;
+
+    case "tie":
+      winner = " Capitalism ";
+      break;
+
+    default:
+      break;
+  }
+  return (
+    <p>
+      Yay.
+      <span className={"subtitle " + props.winner}>{winner}</span>
+      wins.
+    </p>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    // Start with a 7x7 grid full of null
+    let grid = [];
+    for (let rowIndex = 0; rowIndex < gridWidth; rowIndex++) {
+      let row = [];
+      for (let columnIndex = 0; columnIndex < gridWidth; columnIndex++) {
+        if (columnIndex === 0 || columnIndex === gridWidth - 1) {
+          row.push("open");
+        } else row.push("locked");
+      }
+      grid.push(row);
+    }
+
+    this.state = {
+      grid: grid,
+      isDone: false,
+      blueIsNext: true,
+      winner: null,
+    };
+  }
+
+  handleClick(rowIndex, columnIndex) {
+    if (this.state.isDone) return;
+
+    let grid = this.state.grid.map((row) => row.slice());
+    let currentValue = this.state.blueIsNext ? "blue" : "orange";
+
+    // Click on an open (grass) square while the game is running
+    if (!this.state.isDone && grid[rowIndex][columnIndex] === "open") {
+      let nextState;
+      grid[rowIndex][columnIndex] = currentValue;
+      //this.setState({ grid: grid });
+
+      // Check win condition
+      let winCondition = checkWinCondition(grid, rowIndex, columnIndex);
+      if (winCondition.isWin) {
+        nextState = { grid: grid, isDone: true, winner: currentValue };
+      } else {
+        // Set the board up for the next move
+        nextState = prepareNextMove(
+          grid,
+          rowIndex,
+          columnIndex,
+          this.state.blueIsNext
+        );
+      }
+      this.setState(nextState);
+    }
+  }
+
+  renderSquare(rowIndex, columnIndex) {
+    return (
+      <Square
+        position={"r" + rowIndex + "c" + columnIndex}
+        value={this.state.grid[rowIndex][columnIndex]}
+        onClick={() => this.handleClick(rowIndex, columnIndex)}
+      />
+    );
+  }
+
+  render() {
+    let currentPlayer = this.state.blueIsNext ? "blue" : "orange";
+    let status = this.state.isDone ? (
+      <FinalStatus winner={this.state.winner} />
+    ) : (
+      <TurnStatus currentPlayer={currentPlayer} />
+    );
+
+    // This is less awkward. Less 🐢
+    let grid = this.state.grid.slice(); //Strictly to make the next bit readable
+    let gameBoard = grid.map((_, rowIndex) => (
+      <div key={"r" + rowIndex} className="board-row">
+        {grid[rowIndex].map((_, cloumnIndex) =>
+          this.renderSquare(rowIndex, cloumnIndex)
+        )}
+      </div>
+    ));
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="game-board">{gameBoard}</div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <Board />
+      </div>
+    );
+  }
+}
+
+// ========================================
+ReactDOM.render(<Game />, document.getElementById("root"));
