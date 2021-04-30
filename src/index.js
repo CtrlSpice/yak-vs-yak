a3da30995b455d2891d8508b3eb8948515e100af
Amelia Ardath <amelia.ardath@gmail.com>
2021-04-30 13:30:26 -0700

    another deploy attempt

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import StartMenu from "./components/startMenu.js";
 import Board from "./components/board.js";
 import socketClient from "socket.io-client";
 
-const hostname = "yak-vs-yak.herokuapp.com"; //"localhost";
+const hostname = "0.0.0.0"; //"localhost";
 const port = process.env.PORT | 4000;
 
 class Game extends React.Component {
